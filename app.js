7247096200479d0cac0db614923232f5b0525b79
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2020-06-21 20:01:17 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//           (chapter 21 to 25)
+
+        // (Task 1)
+// var fstName = prompt("Enter Your First Name");
+// var lstName = prompt("Enter Your Last Name");
+// var fulName = document.write("Welcome on our website " + fstName + " " + lstName );
+
+       // (Task 2)           ***************Incomplete*****************
+// var favMob = prompt("Enter your favorite mobile phone model");
+
+       // (Task 3)
