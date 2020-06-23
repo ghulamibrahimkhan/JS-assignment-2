56963b778bdba2c93db50721d5d8b939259db498
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2020-06-23 13:58:36 +0500

    chap 21 to 25 update 1

--- app.js
+++ app.js
@@ -7,5 +7,26 @@
 
        // (Task 2)           ***************Incomplete*****************
 // var favMob = prompt("Enter your favorite mobile phone model");
+// var favMobLength = favMob.length
+// document.write("My favourite phone is: " + favMob + "<br>" + "Length of string: " +  favMobLength);
 
        // (Task 3)
+// var str = "Pakistani";
+// var strLength = str.length;
+// document.write("String: Pakistani" + "<br>");
+// for(var i = 0; i < strLength; i++){
+//        if(str.slice(i, i + 1) === "n"){
+//               document.write("Index of 'n': " + i )
+//               break
+//        }
+// }
+
+       // (Task 4)
+// var HeWl = "Hello World"
+// document.write("String: " + HeWl + "<br>");
+// var lstIndex = HeWl.lastIndexOf("l")
+// document.write("Last index of 'l': "  + lstIndex);
+
+       // (Task 5)
+var str = "Pakistani";
+document.write("String: Pakistani" + "<br>");
