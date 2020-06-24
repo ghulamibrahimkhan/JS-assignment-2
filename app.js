fb466e0a1f5c9c543f22772e0354f110c119d4e9
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2020-06-24 05:58:56 +0500

    chap 21 to 25 update 2

--- app.js
+++ app.js
@@ -28,5 +28,52 @@
 // document.write("Last index of 'l': "  + lstIndex);
 
        // (Task 5)
-var str = "Pakistani";
-document.write("String: Pakistani" + "<br>");
+// var str = "Pakistani";
+// document.write("<h1>" + "String: Pakistani" + "<br>");
+// var thirdIndex = str.slice(3,4);
+// document.write("Character at index 3: " + thirdIndex + "</h1>" );
+
+       // (Task 6)  Skipped
+
+
+       // (Task 7)
+// var city = "Hyderabad";
+// document.write("City: " + city + "<br>");
+// var cityReplace = city.replace(" Hyderabad"," Islamabad");
+// document.write("After replacement: " + cityReplace);
+
+       // (Task 8)
+// var message = "Ali and Sami are best friends. They play cricket and football together"
+// var mesgReplace = message.replace(/and/g,"&");
+// document.write(mesgReplace);
+
+       // (Task 9)
+// var str = "472";
+// document.write("Value: " + str + "<br>")
+// document.write("Type: String " + "<br>" + "<br>")
+// var num = parseInt(str);
+// document.write("Value " + num + "<br>")
+// document.write("Type: number " + str + "<br>")
+
+       // (Task 10)
+// var input = prompt("Which Dry Fruits","Peanuts");
+// document.write("User input: " + input + "<br>")
+
+// var outputCapital = input.toUpperCase();
+// document.write("Upper Case: " + outputCapital + "<br>")
+
+       // (Task 11)
+// var inputLan = prompt("Programing Language","javascript");
+// document.write("User input: " + inputLan + "<br>")
+
+// var firstChar = inputLan.slice(0,1);
+// firstChar = firstChar.toUpperCase();
+
+// var otherChars = inputLan.slice(1)
+
+// document.write("Title case: " + firstChar + otherChars + "<br>")
+
+       // (Task 12)
+// var num = 33.36;                       Skipped
+
+       // (Task 13)
