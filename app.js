704535a886f97146ef46d6bdbd2bab780b50b023
ghulamibrahimkhan <gulamibrahimkhan@gmail.com>
2020-06-24 15:54:02 +0500

    chap 26 to 30 update 1

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 //           (chapter 21 to 25)
 
-        // (Task 1)
+// (Task 1)
 // var fstName = prompt("Enter Your First Name");
 // var lstName = prompt("Enter Your Last Name");
 // var fulName = document.write("Welcome on our website " + fstName + " " + lstName );
 
-       // (Task 2)           ***************Incomplete*****************
+// (Task 2)           ***************Incomplete*****************
 // var favMob = prompt("Enter your favorite mobile phone model");
 // var favMobLength = favMob.length
 // document.write("My favourite phone is: " + favMob + "<br>" + "Length of string: " +  favMobLength);
 
-       // (Task 3)
+// (Task 3)
 // var str = "Pakistani";
 // var strLength = str.length;
 // document.write("String: Pakistani" + "<br>");
@@ -21,33 +21,33 @@
 //        }
 // }
 
-       // (Task 4)
+// (Task 4)
 // var HeWl = "Hello World"
 // document.write("String: " + HeWl + "<br>");
 // var lstIndex = HeWl.lastIndexOf("l")
 // document.write("Last index of 'l': "  + lstIndex);
 
-       // (Task 5)
+// (Task 5)
 // var str = "Pakistani";
 // document.write("<h1>" + "String: Pakistani" + "<br>");
 // var thirdIndex = str.slice(3,4);
 // document.write("Character at index 3: " + thirdIndex + "</h1>" );
 
-       // (Task 6)  Skipped
+// (Task 6)  Skipped
 
 
-       // (Task 7)
+// (Task 7)
 // var city = "Hyderabad";
 // document.write("City: " + city + "<br>");
 // var cityReplace = city.replace(" Hyderabad"," Islamabad");
 // document.write("After replacement: " + cityReplace);
 
-       // (Task 8)
+// (Task 8)
 // var message = "Ali and Sami are best friends. They play cricket and football together"
 // var mesgReplace = message.replace(/and/g,"&");
 // document.write(mesgReplace);
 
-       // (Task 9)
+// (Task 9)
 // var str = "472";
 // document.write("Value: " + str + "<br>")
 // document.write("Type: String " + "<br>" + "<br>")
@@ -55,14 +55,14 @@
 // document.write("Value " + num + "<br>")
 // document.write("Type: number " + str + "<br>")
 
-       // (Task 10)
+// (Task 10)
 // var input = prompt("Which Dry Fruits","Peanuts");
 // document.write("User input: " + input + "<br>")
 
 // var outputCapital = input.toUpperCase();
 // document.write("Upper Case: " + outputCapital + "<br>")
 
-       // (Task 11)
+// (Task 11)
 // var inputLan = prompt("Programing Language","javascript");
 // document.write("User input: " + inputLan + "<br>")
 
@@ -73,13 +73,13 @@
 
 // document.write("Title case: " + firstChar + otherChars + "<br>")
 
-       // (Task 12)
+// (Task 12)
 // var num = 33.36;                       Skipped
 
-       // (Task 13)
+// (Task 13)
 
 
-       // (Task 14)
+// (Task 14)
 // var items = ["cake","apple pie","cookie","chips","patties"];
 // var search = prompt("Welcome to our bakery,Search item","cake");
 // search = search.toLowerCase();
@@ -94,10 +94,10 @@
 //        break
 // }
 
-       // (Task 15) skipped 
+// (Task 15) skipped 
 
 
-        // (Task 16)
+// (Task 16)
 // var uni = "University of Karachi"
 // const words = uni.split('');
 // for(i = 0; i < uni.length; i++){
@@ -114,6 +114,48 @@
 //        }
 // }
 
-//         // (Task 18)
+//          (Task 18)
 // var para = "The quick brown fox jumps over the lazy dog";
 // para = para.toLowerCase();
+
+
+//           (chapter 26 to 30)
+
+//          (Task 1)
+
+// var positiveInteger = +prompt();
+// document.write("number: " + positiveInteger + "<br>");
+
+// var roundOff = Math.round(positiveInteger)
+// document.write("round off value: " + roundOff + "<br>");
+
+// var floor = Math.floor(positiveInteger)
+// document.write("floor value: " + floor + "<br>");
+
+// var ceil = Math.ceil(positiveInteger)
+// document.write("ceil value: " + ceil + "<br>");
+
+//          (Task 2)
+
+// var negativeInteger = +prompt();
+
+
+// var signConvert = negativeInteger.toString();
+
+// var signCheck = signConvert.slice(0, 1);
+
+// if (signCheck === "-") {
+//     document.write("number: " + negativeInteger + "<br>");
+
+//     var roundOff = Math.round(negativeInteger)
+//     document.write("round off value: " + roundOff + "<br>");
+
+//     var floor = Math.floor(negativeInteger)
+//     document.write("floor value: " + floor + "<br>");
+
+//     var ceil = Math.ceil(negativeInteger)
+//     document.write("ceil value: " + ceil + "<br>");
+// }
+// else {
+//     alert("Please enter negative value")
+// }
